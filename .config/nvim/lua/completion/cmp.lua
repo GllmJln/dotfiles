64c661f9e88f7810abaa986b4cfde34486f2686d
Thegajout <gjuliens10@gmail.com>
2022-03-28 22:49:09 +0100

    icons for cmp

--- .config/nvim/lua/completion/cmp.lua
+++ .config/nvim/lua/completion/cmp.lua
@@ -1,17 +1,38 @@
 local cmp = require('cmp')
+local cmp_kinds = {
+    Text = "",
+    Method = "ƒ",
+    Function = "ﬦ",
+    Constructor = "",
+    Variable = "",
+    Class = "",
+    Interface = "ﰮ",
+    Module = "",
+    Property = "",
+    Unit = "",
+    Value = "",
+    Enum = "了",
+    Keyword = "",
+    Snippet = "﬌",
+    Color = "",
+    File = "",
+    Folder = "",
+    EnumMember = "",
+    Constant = "",
+    Struct = "",
+}
 
 cmp.setup {
 
     formatting = {
         format = function(entry, vim_item)
             -- set a name for each source
-            vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
+            vim_item.kind = string.format('%s %s', cmp_kinds[vim_item.kind] or "", vim_item.kind)
             vim_item.menu = ({
                 nvim_lsp = "[LSP]",
                 buffer = "[Buffer]",
                 ultisnips = "[UltiSnips]",
                 nvim_lua = "[Lua]",
-                latex_symbols = "[LTX]",
                 look = "[Look]",
                 path = "[Path]",
                 spell = "[Spell]",
@@ -32,7 +53,7 @@ cmp.setup {
     sources = {
         {name = "nvim_lsp"}, {name = "buffer"}, {name = "ultisnips"},
         {name = "nvim_lua"}, {name = "look"}, {name = "path"},
-        {name = "emoji"}, {name = "latex_symbols"}, {name = "spell"}
+        {name = "emoji"} , {name = "spell"}
     },
     completion = {completeopt = 'menu,menuone,noinsert'}
 }
@@ -43,6 +64,7 @@ require("nvim-autopairs.completion.cmp").setup({
     map_complete = true,
     auto_select = true
 })
+
 
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
