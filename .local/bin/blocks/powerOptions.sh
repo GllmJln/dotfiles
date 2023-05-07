4d1bdbc171a3e415ebfdcd1eaee361e6d33ab2f1
GllmJln <gjuliens10@gmail.com>
2023-05-07 18:01:41 +0100

    add shortcut for shutdown selector

--- .local/bin/blocks/powerOptions.sh
+++ .local/bin/blocks/powerOptions.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-
-chosen=$(printf "%s\\nSuspend\\nReboot\\nShutdown" | dmenu -p "Select Power Option:") &&
-case "$chosen" in
-	"Suspend") systemctl suspend ;;
-	"Reboot") reboot ;;
-	"Shutdown") shutdown now ;;
-esac
-
+powerOptions
